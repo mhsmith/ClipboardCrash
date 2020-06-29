1f4e9475e37b70a6eac051625c3936e6c9d78ed6
Malcolm Smith <smith@chaquo.com>
2020-06-29 11:37:01 +0100

    Empty project generated by Android Studio

---
+++ app/src/main/java/com/chaquo/clipboardcrash/MainActivity.java
@@ -0,0 +1,13 @@
+package com.chaquo.clipboardcrash;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
